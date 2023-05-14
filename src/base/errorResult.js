ff1494089b152741eb071669b07c75d439b990d1
vimal.trivedi <vimal.trivedi@tatvasoft.com>
2023-05-14 11:38:19 +0530

    delete js files

--- src/base/errorResult.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.ErrorResult = void 0;
-class ErrorResult {
-    constructor(key) {
-        this.error = key;
-    }
-}
-exports.ErrorResult = ErrorResult;
